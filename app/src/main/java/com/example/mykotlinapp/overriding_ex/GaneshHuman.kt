2ca7c89d93c03e295a9f1cf67f723c768f9ec565
praveen p <ppraveen92@gmail.com>
2020-11-29 00:27:42 +0530

    Overriding Example

---
+++ app/src/main/java/com/example/mykotlinapp/overriding_ex/GaneshHuman.kt
@@ -0,0 +1,14 @@
+package com.example.mykotlinapp.overriding_ex
+
+class GaneshHuman : HumanBeing() {
+    fun drive() = print("I can drive as well")
+    override fun eat() = print("I will eat only veg items")
+    override fun playGames() = print("Play only few games")
+}
+
+fun main(args: Array<String>) {
+    val ganeshHuman = GaneshHuman()
+    ganeshHuman.eat()
+    ganeshHuman.playGames()
+    ganeshHuman.drive()
+}
---
+++ app/src/main/java/com/example/mykotlinapp/overriding_ex/HumanBeing.kt
@@ -0,0 +1,15 @@
+package com.example.mykotlinapp.overriding_ex
+
+/*
+* Overriding
+*
+* */
+open class HumanBeing {
+    open fun eat() {
+        print("Eat All Food Items")
+    }
+
+    open fun playGames() {
+        print("Play all games")
+    }
+}
